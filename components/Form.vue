f94d96e23cedc3154bfc923a8f28a87d9b24b826
Imani Dap <imani.dap@gmail.com>
2019-06-15 16:05:21 +0200

    chore(form): add form component

---
+++ components/Form.vue
@@ -0,0 +1,67 @@
+<template>
+    <form>
+        <h1>Meld je aan voor de introductie</h1>
+        <input type="text" name="firstname" placeholder="Voornaam" aria-label="Voornaam" required>
+        <input type="text" name="prefix" placeholder="Tussenvoegsel" aria-label="Tussenvoegsel">
+        <input type="text" name="lastname" placeholder="Achternaam" aria-label="Achternaam" required>
+        <input type="number" name="studentnumber" placeholder="Studentennummer" aria-label="studentennummer" min="1000000" required>
+        <select name="bachelor" aria-label="Opleiding" required>
+            <option value="">Opleiding</option>
+            <option value="ti">Technische Informatica</option>
+            <option value="inf">Informatica</option>
+            <option value="cmgt">Creative Media and Game Technologies</option>
+            <option value="other">Overig</option>
+        </select>
+        <input type="submit" value="Aanmelden">
+    </form>
+</template>
+
+<style scoped>
+form {
+    display: flex; 
+    flex-direction: column;
+    max-width: 500px;
+    padding: 24px;
+    border-radius: 8px; 
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+}
+
+h1 {
+    font-size: 2em;
+    text-align: center;
+    margin-left: 16px;
+    margin-right: 16px;
+}
+
+input, select {
+    margin-top: 16px;
+    height: 40px;
+    width: 100%;
+    padding: 8px;
+    background-color: #f5f5f5;
+    border-radius: 8px;    
+    
+}
+
+select {
+    appearance: none;
+    background-image: url('/drop-down-arrow.png');
+    background-position: 98%;
+    background-repeat: no-repeat;
+    background-size: 2%; 
+}
+
+input[type="submit"] {
+    background-color: #cf1046;
+    color: #ffffff;
+}
+
+input[type="number"] {
+    -moz-appearance: textfield;
+}
+
+input[type="number"]::-webkit-inner-spin-button, 
+input[type="number"]::-webkit-outer-spin-button {
+    appearance: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        dresscode
-      </h1>
-      <h2 class="subtitle">
-        My stunning Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+  <main>
+    <div class="container">
+      <div>
+        <logo />
+        <h1 class="title">
+          dresscode
+        </h1>
+        <h2 class="subtitle">
+          My stunning Nuxt.js project
+        </h2>
+        <div class="links">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green"
+          >Documentation</a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey"
+          >GitHub</a>
+        </div>    
       </div>
     </div>
-  </div>
+    <section id="registration" aria-label="aanmelden">
+      <Form />
+    </section>
+  </main>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Form from '~/components/Form.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Form
   }
 }
 </script>
@@ -47,6 +54,7 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  max-width: 100vw;
 }
 
 .title {
@@ -54,7 +62,7 @@ export default {
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 4em;
   color: #35495e;
   letter-spacing: 1px;
 }
@@ -69,5 +77,11 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+#registration {
+  display: flex;
+  justify-content: center;
+  padding: 32px; 
 }
 </style>
